183b0d9fda8b0236c2e79a01d9f08db68aee1fbf
Noteverlasting <ofballester@gmail.com>
2025-06-17 09:07:47 +0200

    segunda

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-const express = require('express');
+// DEPENDENCIAS DEL PROYECTO
+const path = require("node:path");
+const express = require("express");
+const morgan = require("morgan");
 const server = express();
-const util = require('node:util');
-const path = require('node:path')
+// const util = require("node:util");
+const fs = require("node:fs");
+require('dotenv').config();
+
+
+// LECTURA DEL FICHERO CON LAS VISITAS ACTUALIZADAS
+const visitasJSON = require("../visitas.json");
+let visitas = visitasJSON["visitas"];
 
 // Le vamos a indicar que la constante puerto la lea desde el archivo .env con process. Si no hubiese puerto definido en .env, será el 30000
 process.loadEnvFile()
@@ -10,22 +19,37 @@ const PUERTO = process.env.PUERTO || 30000;
 // Debemos indicarle a express que use la ruta de fichero estatico que hemos creado
 server.use(express.static(path.join(__dirname, '../public')))
 
+// Middleware de librería
+// server.use(morgan("dev"));
+// Middleware personalizado
+server.use((req, res, next) => {
+  visitas++;
+  let objetoVisitas = `{"visitas": ${visitas}}`;
+  fs.writeFileSync("visitas.json", objetoVisitas, (err) => {
+    if (err) throw err;
+    console.log("Fichero actualizado");
+  });
+  next();
+});
+
+
 // asi pedimos al servidor.
 // En los parentesis, el primer parámetro es el path y el segundo es el método.
 server.get("/", (req, res) => {
     console.log(util.styleText("green","Conectados a HOME"));
     // clg para mostrar por consola el directorio en el que se encuentra el archivo de origen
     console.log(util.styleText("blue", __dirname));
-    // res.send indica a la respuesta que envie algo
-    res.send("<h1>Estamos en la ruta raíz o home</h1>")
-
+    let mensaje = "<h1>Estamos en la ruta raiz o home</h1>";
+    mensaje += `Total visitas: ${visitas}`;
+    // res.send indica a la respuesta que envie algo  
+    res.send(mensaje);
 })
 
 server.get("/clientes", (req, res) => {
-    // res.send indica a la respuesta que envie algo
-    res.send("Estamos en la ruta clientes")
-
-})
+  let mensaje = "<h1>Estamos en la ruta clientes</h1>";
+  mensaje += `Total visitas: ${visitas}`;
+  res.send(mensaje);
+});
 
 server.get("/api/cliente", (req, res) => {
     res.json({"nombre": "James", "apellido": "Pond", "edad": 30})
